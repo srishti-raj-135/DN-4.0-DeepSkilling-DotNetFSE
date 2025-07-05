d1891858517df136987fe2078b9fa87d1438f804
Srishti <srishti0913dkns@gmail.com>
2025-07-05 16:07:04 +0530

    Create AppDbContext.cs

---
+++ Week 3 - 6365294/Entity Framework Core/Lab 1/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace lab1
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Lab1RetailDb;Trusted_Connection=True;");
+        }
+    }
+}
