6b67fde8e599e24bb294026d44238820b35e8cd2
Srishti <srishti0913dkns@gmail.com>
2025-07-05 16:07:44 +0530

    Create Category.cs

---
+++ Week 3 - 6365294/Entity Framework Core/Lab 1/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace lab1
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
