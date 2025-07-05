9f193b6db56a36fc6d08cc082ae12bf7e79be9e4
Srishti <srishti0913dkns@gmail.com>
2025-07-05 16:08:14 +0530

    Create Product.cs

---
+++ Week 3 - 6365294/Entity Framework Core/Lab 1/Product.cs
@@ -0,0 +1,14 @@
+﻿using lab1;
+
+namespace lab1
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
