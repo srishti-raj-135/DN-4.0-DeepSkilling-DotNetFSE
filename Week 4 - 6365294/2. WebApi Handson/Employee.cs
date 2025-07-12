f969b43c7470bf1ebcf7723fa641fdccf08b6d5b
Srishti <srishti0913dkns@gmail.com>
2025-07-13 00:40:20 +0530

    Create Employee.cs

---
+++ Week 4 - 6365294/2. WebApi Handson/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi_Handson
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
