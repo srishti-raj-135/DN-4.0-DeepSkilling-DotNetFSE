8dfb557970a3b8319cced726fcc17af2475167f5
Srishti <srishti0913dkns@gmail.com>
2025-07-13 00:49:19 +0530

    Create CustomExceptionFilter.cs

---
+++ Week 4 - 6365294/3. WebApi Handson/CustomExceptionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi_Handson.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            Directory.CreateDirectory(path);
+            string filePath = Path.Combine(path, "exceptions.txt");
+
+            File.AppendAllText(filePath, $"[{DateTime.Now}] {context.Exception.Message}{Environment.NewLine}");
+
+            context.Result = new ObjectResult("An unexpected error occurred.")
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+}
