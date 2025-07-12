d6af706478420c86241781b86caf29c49bfd026a
Srishti <srishti0913dkns@gmail.com>
2025-07-13 00:52:40 +0530

    Create Employee.cs

---
+++ Week 4 - 6365294/3. WebApi Handson/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApi_Handson
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public bool Permanent { get; set; }
+        public Department Department { get; set; }
+        public List<Skill> Skills { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
