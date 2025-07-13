1c95f8a7aa7dd1d3f0f8fe2b8a88a5576027ed21
Srishti <srishti0913dkns@gmail.com>
2025-07-13 14:17:43 +0530

    Create EmployeeData.cs

---
+++ Week 4 - 6365294/4. WebApi_Handson/EmployeeData.cs
@@ -0,0 +1,23 @@
+﻿namespace _4._WebApi_Handson
+{
+    public static class EmployeeData
+    {
+        public static List<Employee> Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = 1,
+                Name = "Alice",
+                Salary = 50000,
+                Permanent = true,
+                DateOfBirth = new DateTime(1990, 5, 10),
+                Department = new Department { Id = 101, Name = "IT" },
+                Skills = new List<Skill>
+                {
+                    new Skill { Id = 1, Name = "C#" },
+                    new Skill { Id = 2, Name = "SQL" }
+                }
+            }
+        };
+    }
+}
