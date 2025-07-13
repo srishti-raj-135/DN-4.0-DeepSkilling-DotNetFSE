fdc03a7496b48541b64964bb0d702d84eef3921d
Srishti <srishti0913dkns@gmail.com>
2025-07-13 14:22:01 +0530

    Create AuthController.cs

---
+++ Week 4 - 6365294/5. WebApi_Handson/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace _5._WebApi_Handson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            var token = GenerateJSONWebToken(101, "Admin"); // hardcoded user
+            return Ok(new { token });
+        }
+
+        private string GenerateJSONWebToken(int userId, string userRole)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecretkeythatismorethan32chars"));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Role, userRole),
+                new Claim("UserId", userId.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "mySystem",
+                audience: "myUsers",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(2),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
