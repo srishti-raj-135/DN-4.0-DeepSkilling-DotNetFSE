e72aabcb3430583187fad3c4a6810a7038d6d88d
Srishti <srishti0913dkns@gmail.com>
2025-07-19 23:47:14 +0530

    Create Consumer.cs

---
+++ Week 5 - 6365294/KafkaConsoleChatApp/Consumer.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+
+public class Consumer
+{
+    public void StartConsuming(string topic)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+        consumer.Subscribe(topic);
+
+        Console.WriteLine("Listening for messages...");
+        while (true)
+        {
+            var cr = consumer.Consume();
+            Console.WriteLine($"Received: {cr.Message.Value}");
+        }
+    }
+}
