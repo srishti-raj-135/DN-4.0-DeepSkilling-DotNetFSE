0c0b795a7134c370de70bfdd28eec6d67d878d0b
Srishti <srishti0913dkns@gmail.com>
2025-07-19 23:49:37 +0530

    Create Program.cs

---
+++ Week 5 - 6365294/KafkaConsoleChatApp/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var producer = new Producer();
+        var consumer = new Consumer();
+        var topic = "chat-topic";
+
+        Console.Write("Enter username: ");
+        string user = Console.ReadLine();
+
+        Task.Run(() => consumer.StartConsuming(topic));
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            await producer.SendMessage(topic, user, msg);
+        }
+    }
+}
