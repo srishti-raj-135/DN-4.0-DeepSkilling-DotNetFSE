0a416046ede97151a216a24e7ae51ffb2fc0151a
Srishti <srishti0913dkns@gmail.com>
2025-07-20 00:54:17 +0530

    Create AuthController.cs

---
+++ Week 5 - 6365294/Micorservices_q1/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microservices_q1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Microservices_q1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AuthController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (IsValidUser(model))
+            {
+                var token = GenerateJwtToken(model.Username);
+                return Ok(new { Token = token });
+            }
+            return Unauthorized();
+        }
+
+        private bool IsValidUser(LoginModel model)
+        {
+            return model.Username == "test" && model.Password == "password";
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(Convert.ToDouble(_config["Jwt:DurationInMinutes"])),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
