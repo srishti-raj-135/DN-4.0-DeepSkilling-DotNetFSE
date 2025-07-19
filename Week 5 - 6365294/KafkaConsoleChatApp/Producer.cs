445c7b75e7e6dbad5bd7f521a09f0cf183a16196
Srishti <srishti0913dkns@gmail.com>
2025-07-19 23:49:06 +0530

    Create Producer.cs

---
+++ Week 5 - 6365294/KafkaConsoleChatApp/Producer.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+public class Producer
+{
+    public async Task SendMessage(string topic, string user, string message)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+        using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+        var fullMessage = $"{user}: {message}";
+        await producer.ProduceAsync(topic, new Message<Null, string> { Value = fullMessage });
+
+        Console.WriteLine($"Sent: {fullMessage}");
+    }
+}
