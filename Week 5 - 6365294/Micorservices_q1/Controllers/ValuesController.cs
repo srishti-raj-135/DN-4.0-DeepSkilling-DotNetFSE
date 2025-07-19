8993dcaff12e24e49dfb4fb505642e97a3a3324a
Srishti <srishti0913dkns@gmail.com>
2025-07-20 00:55:01 +0530

    Create ValuesController.cs

---
+++ Week 5 - 6365294/Micorservices_q1/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices_q1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("public")]
+        public IActionResult Public() => Ok("Public access OK!");
+
+        [Authorize]
+        [HttpGet("secure")]
+        public IActionResult Secure() => Ok($"Secure access OK! Welcome {User.Identity.Name}");
+    }
+}
