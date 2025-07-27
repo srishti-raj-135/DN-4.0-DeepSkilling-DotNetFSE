0dd06184f7754461e8fea316aa1f201453cd9d48
Srishti <srishti0913dkns@gmail.com>
2025-07-27 22:32:03 +0530

    Create App.js

---
+++ Week 6 - 6365294/studentapp/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+
+
+function App() {
+  return (
+    <center><div className="container">
+      <Home />
+      <About />
+      <Contact />
+    </div></center>
+  );
+}
+
+export default App;
