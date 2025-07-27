373ec39dc9731bd0c3426245b4d3d7ba826809fd
Srishti <srishti0913dkns@gmail.com>
2025-07-27 22:30:55 +0530

    Create Home.js

---
+++ Week 6 - 6365294/studentapp/src/components/Home.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Home extends Component {
+render() {
+    return (
+    <div>
+        <h3>Welcome to Home Page of Student Management Portal</h3>
+    </div>
+    );
+}
+}
+
+export default Home;
