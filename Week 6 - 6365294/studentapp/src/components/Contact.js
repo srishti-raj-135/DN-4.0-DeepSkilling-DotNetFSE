bdeb884e5ef6193e10a64855c052c3cb47b204cb
Srishti <srishti0913dkns@gmail.com>
2025-07-27 22:30:24 +0530

    Create Contact.js

---
+++ Week 6 - 6365294/studentapp/src/components/Contact.js
@@ -0,0 +1,14 @@
+
+import { Component } from 'react';
+
+class Home extends Component {
+render() {
+    return (
+    <div>
+        <h3>Welcome to Contact Page of Student Management Portal</h3>
+    </div>
+    );
+}
+}
+
+export default Home;
