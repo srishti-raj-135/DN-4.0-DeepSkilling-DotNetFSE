c538f3ebfd0d257350e8f5fcba8e52ad416603c4
Srishti <srishti0913dkns@gmail.com>
2025-08-03 12:15:35 +0530

    Create IndianPlayers.js

---
+++ Week 7 - 6365294/cricketapp/src/components/IndianPlayers.js
@@ -0,0 +1,5 @@
+
+export const T20Players = ['Rohit', 'Gill', 'Kohli'];
+export const RanjiTrophyPlayers = ['Sarfaraz', 'Pujara', 'Sundar'];
+
+export const IndianPlayers = [...T20Players, ...RanjiTrophyPlayers];
