24d04cb18f0c8d880ca35f9c2ed35d8dcc359627
Srishti <srishti0913dkns@gmail.com>
2025-08-03 13:25:41 +0530

    Create Greeting.js

---
+++ Week 7 - 6365294/ticketbookingapp/src/Greeting.js
@@ -0,0 +1,9 @@
+
+
+export function GuestGreeting() {
+return <h2>Please sign up to book tickets.</h2>;
+}
+
+export function UserGreeting() {
+return <h2>Welcome back! You can book tickets now.</h2>;
+}
