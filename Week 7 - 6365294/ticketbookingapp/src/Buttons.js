ad94debd86e01519ee626b7965664014e19a72d6
Srishti <srishti0913dkns@gmail.com>
2025-08-03 13:25:17 +0530

    Create Buttons.js

---
+++ Week 7 - 6365294/ticketbookingapp/src/Buttons.js
@@ -0,0 +1,9 @@
+
+
+export function LoginButton({ onClick }) {
+return <button onClick={onClick}>Login</button>;
+}
+
+export function LogoutButton({ onClick }) {
+return <button onClick={onClick}>Logout</button>;
+}
